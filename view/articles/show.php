69182a7e9fd34a7c74cddef5405430b484d659e2
beryld <deback.beryl@gmail.com>
2022-03-22 19:28:17 +0100

    Edit : Correct prev and next variables

--- view/articles/show.php
+++ view/articles/show.php
@@ -1,9 +1,11 @@
 <?php require 'View/includes/header.php'?>
 
-<?php ;// Use any data loaded in the controller here ?>
+<?php
+    $thePreviousIndex = $articles[$_GET['id']-2];
+    $theNextIndex = $articles[$_GET['id']];
+?>
 
     <section>
-
         <h1><?= $articles[$_GET['id']-1]->title;?></h1>
         <p><?= $articles[$_GET['id']-1]->formatPublishDate() ?></p>
         <p><?= $articles[$_GET['id']-1]->description ?></p>
@@ -12,9 +14,12 @@
         <?php // TODO: links to next and previous
         echo($articles[$_GET['id']-1]->id); ?>
         <?php //TODO: NEED THE CNODITIONS IF UNDER 0
+
+//        if ()
+
         // ?>
-        <a href="index.php?page=articles-show&id=<?php echo($articles[$_GET['id']-2]->id) ?>">Previous article</a>
-        <a href="index.php?page=articles-show&id=<?php echo($articles[$_GET['id']+1]->id) ?>">Next article</a>
+        <a href="index.php?page=articles-show&id=<?php echo$thePreviousIndex->id ?>">Previous article</a>
+        <a href="index.php?page=articles-show&id=<?php echo $theNextIndex->id ?>">Next article</a>
     </section>
 
 <?php require 'View/includes/footer.php'?>
